f986535f982e371d59b48a09fee8aff21ce57b0d
lucas farias <lucas.farias@localhost.localdomain>
2018-09-12 22:19:41 -0300

    created folder welcome component by ng g c

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrls: ['./welcome.component.css']
+})
+export class WelcomeComponent {
+
+}
+
+
+//comando para criar component via cli: ng g c 'nome-component --spec=false'
