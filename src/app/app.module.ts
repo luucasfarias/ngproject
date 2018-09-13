d388fbb1a569baaf8e383dbbf764bb48a6999895
lucas farias <lucas.farias@localhost.localdomain>
2018-09-12 22:18:55 -0300

    add new components to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
